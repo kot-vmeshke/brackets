47caf453982d25e8eecc8f8d6b428da0648a32f8
kot-vmeshke <kot-vmeshke.4@gmail.com>
2021-08-29 17:05:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let temp = bracketsConfig.flat();
+  let openBracket = temp.filter((a, b) => !(b%2));
+
+  let config = {};
+
+  for (let i = 0; i < temp.length; i+=2) {
+    let key = temp[i+1];
+    let value = temp[i];
+    config[key] = value;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let symbol = str[i];
+    
+    if (openBracket.includes(symbol)) {
+      stack.push(symbol);
+
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topSymbol = stack[stack.length-1];
+      if (config[symbol] === topSymbol) {
+        stack.pop();
+      } else { return false }
+    }
+  }
+
+  return stack.length === 0;
 }
