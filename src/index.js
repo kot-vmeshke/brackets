77051027f92b0292ab816de86bbf7f60b978368f
Anna Babay <kot-vmeshke.4@gmail.com>
2022-07-16 16:23:39 +0200

    fix: for ||, 77, 88

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
+
   let stack = [];
   let temp = bracketsConfig.flat();
-  let openBracket = temp.filter((a, b) => !(b%2));
+  let openBracket = temp.filter((a, b) => !(b % 2));
 
   let config = {};
 
-  for (let i = 0; i < temp.length; i+=2) {
-    let key = temp[i+1];
+  for (let i = 0; i < temp.length; i += 2) {
+    let key = temp[i + 1];
     let value = temp[i];
     config[key] = value;
   }
 
   for (let i = 0; i < str.length; i++) {
     let symbol = str[i];
-    
     if (openBracket.includes(symbol)) {
       stack.push(symbol);
-
+      if (stack[stack.length - 1] === '|' || stack[stack.length - 1] === '7' || stack[stack.length - 1] === '8') {
+        if (stack[stack.length - 1] === stack[stack.length - 2]) {
+          stack.splice(stack.length - 2, 2)
+        }
+      }
     } else {
       if (stack.length === 0) {
         return false;
       }
-      let topSymbol = stack[stack.length-1];
+      let topSymbol = stack[stack.length - 1];
       if (config[symbol] === topSymbol) {
         stack.pop();
       } else { return false }
